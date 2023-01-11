b0a1e05a8abe82e7925bd8224623cfd4c9887c0b
GitQaed <gitqaed_@outlook.fr>
2023-01-11 12:06:14 +0100

    request/exo

--- src/exo.ts
+++ src/exo.ts
@@ -7,12 +7,16 @@ const note: number[] = [a, b, 12];
 
 console.table(note);
 
-const ordonnerNotes = (tableauDeNotes: number[]): number[] => {
-  let noteSort = tableauDeNotes.sort((a, b) => a - b);
-  return noteSort;
-};
+// const ordonnerNotes = (tableauDeNotes: number[]): number[] => {
+//   let newNotes = [...tableauDeNotes];
+//   newNotes.sort((a, b) => a - b);
+//   return newNotes;
+// };
+
+const ordonnerNotes = (notes: number[]) => [...notes].sort((a, b) => a - b);
 
 console.table(ordonnerNotes(note));
+// console.table(note)
 
 type Student = {
   nom: string;
@@ -21,19 +25,22 @@ type Student = {
   notes: number[];
 };
 
-const student = {
+const john: Student = {
   nom: "Doe",
   prenom: "john",
   age: 45,
   notes: note,
 };
 
-console.table(student);
+console.table(john);
 
-const AfficherStudent = (student: Student): string => {
-  return `Bonjour ${student.prenom} ${student.nom}, vous avez ${student.age} ans `;
-};
-console.log(AfficherStudent(student));
+// const AfficherStudent = (student: Student): string =>
+//   `Bonjour ${student.prenom} ${student.nom}, vous avez ${student.age} ans `;
+
+const AfficherStudent = ({ nom, prenom, age }: Student): string =>
+  `Bonjour ${prenom} ${nom}, vous avez ${age} ans `;
+
+console.log(AfficherStudent(john));
 
 const AfficheNote = (tableauNotes: number[]): void => {
   tableauNotes.forEach((valeur, index) => {
@@ -42,23 +49,35 @@ const AfficheNote = (tableauNotes: number[]): void => {
 };
 AfficheNote(note);
 
-const moyenne = (tableauDeNotes: number[]): number => {
-  let total = 0;
-  for (let i = 0; i < tableauDeNotes.length; i++) {
-    total += tableauDeNotes[i];
-  }
-  return total / tableauDeNotes.length;
-};
+// const moyenne = (tableauDeNotes: number[]): number => {
+//   let total = 0;
+//   for (let i = 0; i < tableauDeNotes.length; i++) {
+//     total += tableauDeNotes[i];
+//   }
+//   return total / tableauDeNotes.length;
+// };
+
+const moyenne = (notes: number[]): number =>
+  notes.reduce((total, note) => total + note, 0) / notes.length;
+
 console.log(`la moyenne des notes est: ${moyenne(note)}`);
 
-type ProfPrincipal = {
-  text?: string;
-  objet?: {
-    nom: string;
-    prenom: string;
-    matiere: string;
-  };
-};
+// type ProfPrincipal = {
+//   nom?: string;
+//   identite?: {
+//     nom: string;
+//     prenom: string;
+//     matiere: string;
+//   };
+// };
+
+type ProfPrincipal =
+  | string
+  | {
+      nom: string;
+      prenom: string;
+      matiere: string;
+    };
 
 type Identifiable<A> = {
   id: A;
